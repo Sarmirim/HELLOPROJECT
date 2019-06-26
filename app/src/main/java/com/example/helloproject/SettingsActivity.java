9ab65df21b8ec62524002c0e9774b817be6d1db8
Alex <Sarmirim@gmail.com>
2019-06-26 18:39:49 +0300

    Общая работа над цветами

--- app/src/main/java/com/example/helloproject/SettingsActivity.java
+++ app/src/main/java/com/example/helloproject/SettingsActivity.java
@@ -23,6 +23,7 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
