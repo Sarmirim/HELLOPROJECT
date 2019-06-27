02c049f8063467e5bfc3f2166651c1ef3e94a74d
Alex <Sarmirim@gmail.com>
2019-06-27 16:27:20 +0300

    colors bla-bla-bla

--- app/src/main/java/com/example/helloproject/AboutActivity.java
+++ app/src/main/java/com/example/helloproject/AboutActivity.java
@@ -6,9 +6,18 @@ import android.os.Bundle;
 
 public class AboutActivity extends AppCompatActivity {
 
+    private static final String[] aboutString = {"credits" };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+
+
     }
+
+
+
 }
