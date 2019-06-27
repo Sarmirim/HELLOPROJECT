f98fb6e6da29b3c7a310595da99229347cfd4b93
Alex <Sarmirim@gmail.com>
2019-06-27 14:32:21 +0300

    +About Activity (Blank)

--- app/src/main/java/com/example/helloproject/MainActivity.java
+++ app/src/main/java/com/example/helloproject/MainActivity.java
@@ -100,6 +100,9 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
+        } else if (id == R.id.nav_about) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
